229558cfcf42072deaed0ec43ae77b9e43a18131
Aditya Aneesh <adithya.aneesh59421@gmail.com>
2025-06-25 20:49:01 +0400

    Remade Node

--- Assets/Scripts/PathFinding/Node.cs
+++ Assets/Scripts/PathFinding/Node.cs
@@ -2,28 +2,30 @@ using UnityEngine;
 
 namespace PathFinding
 {
-    public class Node {
-        public bool walkable;
-        public Vector3 worldPosition;
-        public int gridX, gridY;
+    public class Node
+    {
+        // Each block in a grid to form a path
+        
+        public readonly bool Walkable; // Check is the node is obstructed
+        public Vector3 WorldPosition; // Node position
+        public readonly int GridX; // Position in grid X
+        public readonly int GridY; // Position in grid Y
 
-        public int gCost;
-        public int hCost;
-        public Node parent;
+        private int _gCost; //  Cost from start node to the current node following parents path
+        private int _hCost; // Heuristic cost from current node to goal node
+        public Node Parent; // 
 
-        public int FCost
+        public int FCost => _gCost + _hCost; // adds both costs to find next path to search
+
+        public Node(bool walkable, Vector3 worldPos, int gridX, int gridY)
         {
-            get
-            {
-                return gCost + hCost;
-            }
-        }
-
-        public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY) {
-            walkable     = _walkable;
-            worldPosition = _worldPos;
-            gridX        = _gridX;
-            gridY        = _gridY;
+            // Initializing variables in constructor
+            Walkable = walkable;
+            WorldPosition = worldPos;
+            GridX = gridX;
+            GridY = gridY;
+            _gCost = 0;
+            _hCost = 0;
         }
     }
 }
