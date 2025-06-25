00af033e106d8b9ad57a7aa1d498af1a519bfe6b
Aditya Aneesh <adithya.aneesh59421@gmail.com>
2025-06-25 21:10:08 +0400

    pseudocode for a* algorithm

--- Assets/Scripts/PathFinding/AStar.cs
+++ Assets/Scripts/PathFinding/AStar.cs
@@ -1,87 +1,38 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace PathFinding
 {
-    public class AStar: MonoBehaviour {
-        public Transform seeker, target;
-        Grid grid;
+    public class AStar : MonoBehaviour
+    {
+        //open list
+        //closed list
+        //start node
+        //end node
+        //neighbour list
+        //current node
 
-        void Awake() {
-            grid = GetComponent<Grid>();
-        }
+        //start
+        // append start node to open list
 
-        void Update() {
-            FindPath(seeker.position, target.position);
-        }
+        //update
+        // while true loop
+        //   sort open list
+        //   current node is the first value in the open list
+        //   move current node from open to closed
 
-        void FindPath(Vector3 startPos, Vector3 targetPos) {
-            Node startNode  = grid.NodeFromWorldPoint(startPos);
-            Node targetNode = grid.NodeFromWorldPoint(targetPos);
+        // if the current node is target node, break look
 
-            List<Node> openSet = new List<Node>();
-            HashSet<Node> closedSet = new HashSet<Node>();
+        // find neighbours of the current node and add it to a list
 
-            openSet.Add(startNode);
+        // loop through the neighbors
+        // if the neighbour node is not walkable or is in the closed list. continue the loop
 
-            while (openSet.Count > 0) {
-                Node currentNode = openSet[0];
-                for (int i = 1; i < openSet.Count; i++) {
-                    if (openSet[i].fCost < currentNode.fCost ||
-                        (openSet[i].fCost == currentNode.fCost 
-                         && openSet[i].hCost < currentNode.hCost)) {
-                        currentNode = openSet[i];
-                    }
-                }
+        // if the neighbour node is not in open list or its g cost (or f cost since h cost does not change) 
+        //   update the gcost and hcost
+        //   set parent of the neighbour node to be the current
+        //   if the neighbour is not in the open list, add it to the open list
 
-                openSet.Remove(currentNode);
-                closedSet.Add(currentNode);
 
-                if (currentNode == targetNode) {
-                    RetracePath(startNode, targetNode);
-                    return;
-                }
-
-                foreach (Node neighbour in grid.GetNeighbours(currentNode)) {
-                    if (!neighbour.walkable || closedSet.Contains(neighbour))
-                        continue;
-
-                    int newGCost = currentNode.gCost + GetDistance(currentNode, neighbour);
-
-                    if (newGCost < neighbour.gCost || !openSet.Contains(neighbour)) {
-                        neighbour.gCost = newGCost;
-                        neighbour.hCost = GetDistance(neighbour, targetNode);
-                        neighbour.parent = currentNode;
-
-                        if (!openSet.Contains(neighbour))
-                            openSet.Add(neighbour);
-                    }
-                }
-            }
-        }
-
-        void RetracePath(Node startNode, Node endNode) {
-            List<Node> path = new List<Node>();
-            Node current = endNode;
-
-            while (current != startNode) {
-                path.Add(current);
-                current = current.parent;
-            }
-            path.Reverse();
-
-            // move the enemy
-        }
-
-        int GetDistance(Node a, Node b) {
-            int dstX = Mathf.Abs(a.gridX - b.gridX);
-            int dstY = Mathf.Abs(a.gridY - b.gridY);
-
-            if (dstX > dstY)
-                return 14*dstY + 10*(dstX - dstY);
-            return 14*dstX + 10*(dstY - dstX);
-        }
     }
 }
 
-}
